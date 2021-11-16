db39729cfc3eb374103c0805e21f32d0ed9d789b
canbalaman <65334876+canbalaman@users.noreply.github.com>
2021-11-16 15:33:54 +0300

    First Commit

---
+++ Example2.sql
@@ -0,0 +1,3 @@
+-- actor tablosunda bulunan first_name ve last_name sütunlardaki verileri first_name 'Penelope' veya 'Nick' veya 'Ed' değerleri olması koşuluyla sıralayınız. ( IN operatörünü kullanınız.) değeri 12.99 dan büyük eşit ve 16.99 küçük olma koşuluyla sıralayınız ( BETWEEN - AND yapısını kullanınız.)
+SELECT *  FROM actor 
+WHERE first_name IN ('Penelope','Nick','Ed');
